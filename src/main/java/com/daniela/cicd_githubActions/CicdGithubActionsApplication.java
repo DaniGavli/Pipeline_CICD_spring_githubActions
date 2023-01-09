202f20cc09d6a94ce641be50f73e636e9a2197c6
DaniGavli <dngg55@gmail.com>
2023-01-09 14:10:05 -0300

    subindo o projeto para o git

---
+++ src/main/java/com/daniela/cicd_githubActions/CicdGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.daniela.cicd_githubActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/daniela/cicd_githubActions/DataController.java
@@ -0,0 +1,87 @@
+package com.daniela.cicd_githubActions;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+
+public class DataController {
+
+    @GetMapping("/")
+
+    public String healthCheck() {
+
+        return "OK OK!!";
+
+    }
+
+    @GetMapping("/version")
+
+    public String version() {
+
+        return "A versão atual é 1.0.0";
+
+    }
+
+    @GetMapping("/nations")
+
+    public JsonNode getRandomNations() {
+
+        var objectMapper = new ObjectMapper();
+
+        var faker = new Faker(new Locale("en-US"));
+
+        var nations = objectMapper.createArrayNode();
+
+        for (var i = 0; i < 10; i++) {
+
+            var nation = faker.nation();
+
+            nations.add(objectMapper.createObjectNode()
+
+                    .put("Nacionalidade", nation.nationality())
+
+                    .put("Capital da cidade", nation.capitalCity())
+
+                    .put("Bandeira", nation.flag())
+
+                    .put("Idioma", nation.language()));
+
+        }
+
+        return nations;
+
+    }
+
+    @GetMapping("/currencies")
+
+    public JsonNode getRandomCurrencies() {
+
+        var objectMapper = new ObjectMapper();
+
+        var faker = new Faker(new Locale("en-US"));
+
+        var currencies = objectMapper.createArrayNode();
+
+        for (var i = 0; i < 20; i++) {
+
+            var currency = faker.currency();
+
+            currencies.add(objectMapper.createObjectNode()
+
+                    .put("Moeda", currency.name())
+
+                    .put("codigo", currency.code()));
+
+        }
+
+        return currencies;
+
+    }
+
+}
